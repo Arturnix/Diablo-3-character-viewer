52880e67f9f091330943b37adc1bf4d80ac41efb
Arturnix <artur.zgodka.programista@gmail.com>
2024-01-21 21:42:08 +0100

    Added show item info using ItemSlugAndId

--- src/main/java/com/diablo3CharViewer/CharacterViewerManager.java
+++ src/main/java/com/diablo3CharViewer/CharacterViewerManager.java
@@ -1,39 +1,54 @@
 package com.diablo3CharViewer;
 
-import com.diablo3CharViewer.api_handlers.AccountHandlerApi;
-import com.diablo3CharViewer.api_handlers.HeroHandlerApi;
-import com.diablo3CharViewer.token.FetchToken;
-
 import java.util.Scanner;
 
 public class CharacterViewerManager {
 
-    public void showProfile(Scanner scanner, AccountHandlerApi accountHandlerApi, FetchToken fetchToken) {
-        System.out.println("Zostan na chwile i poczytaj:\n" + accountHandlerApi.generateRequest(battleTagProvider(scanner), fetchToken) + '\n');
-    }
-
-    public void showHero(Scanner scanner, HeroHandlerApi heroHandlerApi, FetchToken fetchToken) {
-        System.out.println("Zostan na chwile i poczytaj:\n" + heroHandlerApi.generateRequest(battleTagProvider(scanner), heroIdProvider(scanner), fetchToken) + '\n');
-    }
-
-    private String battleTagProvider(Scanner scanner) { //zrobic funkcje anonimowe z tego?
-        System.out.println("Witaj wedrowcze! Podaj battleTag aby wyszukac profil bohatera: ");
-        String battleTag = scanner.nextLine();
-
-        return battleTag;
-    }
-
-    private String heroIdProvider(Scanner scanner) {
-        System.out.println("Podaj heroId aby wyswietlic postac: ");
-        String heroId = scanner.nextLine();
-
-        return heroId;
-    }
-
     public void showMenu() {
+
         System.out.println("1. Przegladaj profil podajac batlleTag bohatera");
         System.out.println("2. Wyswietl postac dla wybranego profilu");
-        System.out.println("3. Opusc archiwum");
+        System.out.println("3. Wyswietl informacje o przedmiocie");
+        System.out.println("4. Opusc archiwum");
+    }
+
+    public void showProfile(GetApiAccount getApiAccount, FetchToken fetchToken) {
+
+        System.out.println("Witaj wedrowcze! Podaj battleTag aby wyszukac profil bohatera: ");
+        Scanner scanner = new Scanner(System.in);
+        String battleTag = scanner.nextLine();
+        scanner.reset();
+        getApiAccount.setAccount(battleTag);
+        getApiAccount.setRequestURL();
+
+        System.out.println("Zostan na chwile i poczytaj:\n" + getApiAccount.generateRequest(fetchToken) + '\n');
+    }
+
+    public void showHero(GetApiHero getApiHero, FetchToken fetchToken) {
+        System.out.println("Witaj wedrowcze! Podaj battleTag aby wyszukac profil bohatera: ");
+        Scanner scanner = new Scanner(System.in);
+        String battleTag = scanner.nextLine();
+        scanner.reset();
+
+        System.out.println("Podaj heroId aby wyswietlic postac: ");
+        String heroId = scanner.nextLine();
+        scanner.reset();
+
+        getApiHero.setAccount(battleTag);
+        getApiHero.setHeroId(heroId);
+        getApiHero.setRequestURL();
+        System.out.println("Zostan na chwile i poczytaj:\n" + getApiHero.generateRequest(fetchToken) + '\n');
+    }
+
+    public void showItem(GetApiItem getApiItem, FetchToken fetchToken) {
+        System.out.println("Witaj wedrowcze! Podaj itemSlugAndId aby wysweitlic informacje o przedmiocie: ");
+        Scanner scanner = new Scanner(System.in);
+        String itemSlugAndId = scanner.nextLine();
+        scanner.reset();
+
+        getApiItem.setItemSlugAndId(itemSlugAndId);
+        getApiItem.setRequestURL();
+        System.out.println("Zostan na chwile i poczytaj:\n" + getApiItem.generateRequest(fetchToken) + '\n');
     }
 
 }
--- src/main/java/com/diablo3CharViewer/Main.java
+++ src/main/java/com/diablo3CharViewer/Main.java
@@ -1,9 +1,5 @@
 package com.diablo3CharViewer;
 
-import com.diablo3CharViewer.api_handlers.AccountHandlerApi;
-import com.diablo3CharViewer.api_handlers.HeroHandlerApi;
-import com.diablo3CharViewer.token.FetchToken;
-
 import java.util.Scanner;
 
 public class Main {
@@ -11,8 +7,9 @@ public class Main {
 
         FetchToken fetchToken = new FetchToken();
         //fetchToken.requestToken();
-        AccountHandlerApi accountHandlerApi = new AccountHandlerApi();
-        HeroHandlerApi heroHandlerApi = new HeroHandlerApi();
+        GetApiAccount getApiAccount = new GetApiAccount();
+        GetApiHero getApiHero = new GetApiHero();
+        GetApiItem getApiItem = new GetApiItem(); //dac tworzenie klas po podaniu battleTag lub item Id z podaną wartoscia dla tego pola
         CharacterViewerManager characterViewerManager = new CharacterViewerManager();
 
         int wybor;
@@ -21,24 +18,28 @@ public class Main {
         System.out.println("Witaj w archiwum bohaterow swiata Sanktuarium!\nWybierz odpowiednia opcje z menu aby przejrzec zapisy archiwum:");
 
        do {
-            characterViewerManager.showMenu();
 
+            characterViewerManager.showMenu();
             wybor = scanner.nextInt();
             scanner.nextLine(); //nextInt() doesnt consume new line char when hitting enter to confirm typed data. So this command consume left end line char.
 
             switch (wybor) {
                 case 1:
-                    characterViewerManager.showProfile(scanner, accountHandlerApi, fetchToken);
+                    characterViewerManager.showProfile(getApiAccount, fetchToken);
                     break;
                 case 2:
-                    characterViewerManager.showHero(scanner, heroHandlerApi, fetchToken);
+                    characterViewerManager.showHero(getApiHero, fetchToken);
                     break;
                 case 3:
+                    characterViewerManager.showItem(getApiItem, fetchToken);
+                    break;
+                case 4:
                     System.out.println("Zegnaj wedrowcze...");
                     break;
                 default:
                     System.out.println("Dokonaj poprawnego wyboru:");
             }
         } while(wybor != 3);
+       scanner.close(); //closable wiec zrobic try with resources
     }
 }
