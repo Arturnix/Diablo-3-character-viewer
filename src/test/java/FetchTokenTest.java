1903c0c67c387763dcbfd5a76849c55c316960aa
Arturnix <artur.zgodka.programista@gmail.com>
2024-01-22 20:36:00 +0100

    Added test - fetch correct token

--- src/test/java/FetchTokenTest.java
+++ src/test/java/FetchTokenTest.java
@@ -1,11 +1,16 @@
+import com.diablo3CharViewer.token.FetchToken;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class FetchTokenTest {
 
-    private FetchTokenTest testObject = new FetchTokenTest();
+    private FetchToken testObject = new FetchToken();
 
     @Test
     public void corrrectTokenFetched() {
 
+        String token = "EU7S6RQ5lRGROtbckZEJMezVfURWyk3yit"; //use actual token value
+
+        Assertions.assertEquals(token, testObject.requestToken().getAccess_token());
     }
 }
