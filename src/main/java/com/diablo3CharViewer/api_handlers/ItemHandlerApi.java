9117bb3ec490c87a1ad0c64c67754f28fcabe59d
Arturnix <artur.zgodka.programista@gmail.com>
2024-01-31 23:12:29 +0100

    Refactor ItemHandlerApi using BaseUrlParts

--- src/main/java/com/diablo3CharViewer/api_handlers/ItemHandlerApi.java
+++ src/main/java/com/diablo3CharViewer/api_handlers/ItemHandlerApi.java
@@ -6,7 +6,10 @@ import com.diablo3CharViewer.token.Token;
 public class ItemHandlerApi {
     //corrupted-ashbringer-Unique_Sword_2H_104_x1
     public String generateRequest(String itemSlugAndId, FetchToken fetchToken) { //token przypisywać ze zmiennej. Pobieram token kiedy program startuje i wrzucam go do stałej.
-        return fetchToken.fetchAPIResourceRequest("https://eu.api.blizzard.com/d3/data/item/" +
-                itemSlugAndId + "?locale=pl_PL&access_token=" + Token.getAccess_token());
+
+        String apiEndpoint = BaseUrlParts.getBaseItemApi() + itemSlugAndId;
+        String localeAndToken = BaseUrlParts.getBaseLocaleAndToken() + Token.getAccess_token();
+
+        return fetchToken.fetchAPIResourceRequest(apiEndpoint + localeAndToken);
     }
 }
