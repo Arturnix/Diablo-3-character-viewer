fe20a046c56effa3e6c3dcea4c3a977e322f22e6
Arturnix <artur.zgodka.programista@gmail.com>
2024-02-01 16:10:20 +0100

    Make final variables upper case

--- src/main/java/com/diablo3CharViewer/api_handlers/BaseUrlParts.java
+++ src/main/java/com/diablo3CharViewer/api_handlers/BaseUrlParts.java
@@ -2,25 +2,25 @@ package com.diablo3CharViewer.api_handlers;
 
 public class BaseUrlParts {
 
-    private static final String baseBlizzardUrl = "https://eu.api.blizzard.com/d3/";
-    private static final String baseProfileApi = baseBlizzardUrl + "profile/";
-    private static final String baseHeroApi = "/hero/";
-    private static final String baseItemApi = baseBlizzardUrl + "data/item/";
-    private static final String baseLocaleAndToken = "/?locale=pl_PL&access_token="; //zawsze wystepuja razem w stringu wiec dalem do jednego stringa
+    private static final String BASE_BLIZZARD_URL = "https://eu.api.blizzard.com/d3/";
+    private static final String BASE_PROFILE_API = BASE_BLIZZARD_URL + "profile/";
+    private static final String BASE_HERO_API = "/hero/";
+    private static final String BASE_ITEM_API = BASE_BLIZZARD_URL + "data/item/";
+    private static final String BASE_LOCALE_AND_TOKEN = "/?locale=pl_PL&access_token="; //zawsze wystepuja razem w stringu wiec dalem do jednego stringa
 
     public static String getBaseProfileApi() {
-        return baseProfileApi;
+        return BASE_PROFILE_API;
     }
 
     public static String getBaseHeroApi() {
-        return baseHeroApi;
+        return BASE_HERO_API;
     }
 
     public static String getBaseItemApi() {
-        return baseItemApi;
+        return BASE_ITEM_API;
     }
 
     public static String getBaseLocaleAndToken() {
-        return baseLocaleAndToken;
+        return BASE_LOCALE_AND_TOKEN;
     }
 }
